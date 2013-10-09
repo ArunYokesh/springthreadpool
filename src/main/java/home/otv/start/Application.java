4f498e817a6ad7745daa8614c8732055c53972d1
Sachdev, J - SPLXE <jagrut.sachdev@klm.com>
2013-10-09 17:20:03 +0200

    comment added

--- src/main/java/home/otv/start/Application.java
+++ src/main/java/home/otv/start/Application.java
@@ -17,6 +17,7 @@ public class Application {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"applicationContext.xml");
+		//start
 		Starter starter = (Starter) context.getBean("Starter");
 		starter.start();
 	}
